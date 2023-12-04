d71f426b0fb1aefc78aa6d479a4abe73a690f2cd
jackielics <jackielics@qq.com>
2023-12-04 10:47:01 +0800

    update

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -7,7 +7,7 @@
 # @lc code=start
 class Solution:
 	# 1. Brute-Force
-	def twoSum0(self, nums: List[int], target: int) -> List[int]:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
 		for i in range(len(nums)):
 			for j in range(i+1, len(nums)):
 				if nums[i] + nums[j] == target:
