7e985bed9aa21db9c6a6441d028511352f524147
jackielics <jackielics@qq.com>
2025-08-21 18:25:51 +0800

    2025-08-21 18:25:51 commited at Mac Air

---
+++ 0-Leetcode-Template.md
@@ -0,0 +1,17 @@
+# Topic
+Python3
+# Intuition
+
+# Approach
+
+# Complexity
+- Time complexity:
+$$O(n)$$
+
+- Space complexity:
+$$O(n)$$
+
+# Code
+```python
+
+```
--- 40.组合总和-ii.py
+++ 40.组合总和-ii.py
@@ -8,4 +8,29 @@
 class Solution:
 	def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 		
+		res = set()
+		curset = [] # current list of elems used
+		def backtrack(i, total):
+			'''
+			i: start index
+			total: current total
+			'''
+			# Bound
+			if total == target: # meet target
+				res.add(tuple(sorted(curset))) # shallow copy(enough for [int])
+				return
+			elif total > target or i >= len(candidates):
+				return
+
+			curset.append(candidates[i])
+			backtrack(i + 1, total + candidates[i]) # try [i]
+			curset.pop()
+			while i < len(candidates) - 1 and candidates[i] == candidates[i + 1]:
+				i += 1
+			backtrack(i + 1, total) # skip [i]
+
+		backtrack(0, 0) # start from i=0, total=0
+		
+		return [list(x) for x in res]
+
 # @lc code=end
