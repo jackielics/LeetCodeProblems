f4f6d97b74494c9203ecddeb3db3e4b76fc92e93
jackielics <jackielics@qq.com>
2025-08-26 23:14:35 +0800

    2025-08-26 23:14:35 commited at Mac Air

--- 210.课程表-ii.py
+++ 210.课程表-ii.py
@@ -1,18 +1,20 @@
 #
-# @lc app=leetcode.cn id=207 lang=python3
+# @lc app=leetcode.cn id=210 lang=python3
 #
-# [207] 课程表
+# [210] 课程表 II
 #
 
 # @lc code=start
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         visited = set()
+        res = []
 
         # initial
         preMap = collections.defaultdict(list)
         for x in prerequisites:
             preMap[x[0]].append(x[1])
+
 
         def dfs(cur):
             if cur in visited: # repeat
@@ -25,12 +27,13 @@ class Solution:
             visited.remove(cur)
 
             preMap.pop(cur) # already reached
+            if cur not in res:
+                res.append(cur)
             return True
         
         for x in range(numCourses):
             if not dfs(x):
-                return False
-        return True
-
+                return []
+        return res
 # @lc code=end
 
