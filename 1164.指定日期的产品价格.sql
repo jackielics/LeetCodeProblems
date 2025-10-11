649912dfc7bfee1bd243e0ff7107c583bf07abe9
jackielics <jackielics@qq.com>
2025-10-11 22:05:11 +0800

    2025-10-11 22:05:11 commited at Mac Air

--- 1164.指定日期的产品价格.sql
+++ 1164.指定日期的产品价格.sql
@@ -6,22 +6,38 @@
 
 -- @lc code=start
 # Write your MySQL query statement below
-select p1.product_id, ifnull(p2.new_price, 10) as price
-from (
-    select distinct product_id
-    from products
-) as p1 -- 所有的产品
-left join (
-    select product_id, new_price 
-    from products
-    where (product_id, change_date) in (
-        select product_id, max(change_date)
-        from products
-        where change_date <= '2019-08-16'
-        group by product_id
-    )
-) as p2 -- 在 2019-08-16 之前有过修改的产品和最新的价格
-on p1.product_id = p2.product_id
+WITH CTE AS
+(SELECT
+    product_id,
+    MAX(change_date) AS change_date
+FROM 
+    Products
+WHERE
+    change_date <= "2019-08-16"
+GROUP BY
+    product_id)
 
+-- 目标日期前没变过价格的
+SELECT 
+    product_id,
+    10 AS price
+FROM 
+    Products
+WHERE 
+    product_id NOT IN (
+        SELECT product_id 
+        FROM CTE
+        )
+UNION 
+SELECT
+    c.product_id,
+    p.new_price
+FROM
+    CTE c
+LEFT JOIN
+    Products p
+ON 
+    c.product_id = p.product_id
+    AND c.change_date = p.change_date
 -- @lc code=end
 
